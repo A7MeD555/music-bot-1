2b79987e47022bb384769d725f16cd9e50ed8b3c
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-10-24 15:48:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ const request = require('request');
 const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
-
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '1';
 client.login(discord_token);
 client.on('ready', function() {
